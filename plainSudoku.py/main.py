c3d24c093d5ef0f229b39671f098bf6ba2cc246d
vikkstar1 <bubupostvitalie@gmail.com>
2022-06-20 20:50:32 +0300

    check if sudoku is valid

--- plainSudoku.py/main.py
+++ plainSudoku.py/main.py
@@ -24,3 +24,28 @@ def draw_sudoku(b):
 
             else:
                 print(str(b[i][j]) + " ", end="")   #prints the last row without a line
+
+def search_empty(b):
+    for i in range(len(b)):
+        for j in range(len(b[0])):
+            if b[i][j] == 0:
+                return (i , j)  #returns the index of the empty cell in the grid
+
+def is_valid(b,num,pos):                
+    for i in range(len(b[0])):                      
+        if b[pos[0]][i] == num and pos[1] != i:            #checks if the number is present in the row
+            return False
+    
+    for i in range(len(b)):
+        if b[i][pos[1]] == num and pos[0] != i:             #checks if the number is present in the column
+            return False
+
+    #check the grid of the given position
+    x_grid = pos[1] // 3                                    #gets the column of the grid(box)
+    y_grid = pos[0] // 3                                    #gets the row of the grid
+    for i in range(y_grid * 3,y_grid *3 +3):                
+        for j in range(x_grid *3, x_grid *3 + 3):
+            if b[i][j] == num and (i,j) != pos:             #checks if the number is present in a grid(box)
+                return False
+
+    return True 
